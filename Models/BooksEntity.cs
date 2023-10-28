e611e42aa1009cf7f86151691bab3cd2892429e5
kitou88C39 <kanfutrooper@icloud.com>
2023-10-28 19:53:49 +0900

    Models/BookEntity.cs 編集

---
+++ Models/BooksEntity.cs
@@ -0,0 +1,11 @@
+namespace AmazonBooks.Models;
+
+public class BooksEntity{
+
+    public int Id{get; set;}
+    public string Title{get; set;}
+
+    public int Author{get; set;}
+    public string ISBN{get; set;}
+
+}
