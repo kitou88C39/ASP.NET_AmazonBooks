61fdb57ada4631ac66186c111bffa1da7cf60d7f
kitou88C39 <kanfutrooper@icloud.com>
2023-10-31 17:14:15 +0900

    Controllers/BookController.cs 編集

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,8 +1,11 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AmazonBooks.Models;
+using AmazonBooks.Data;
 
 namespace AmazonBooks.Controllers;
 
 public class BookController : Controller
-{}
+{
+    private ApplicationDbContext_db;
+}
